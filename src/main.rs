f0c0fd799221c7cbf9a624f46e80de9fc481c051
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-01 06:27:48 +0100

    Add spawning people

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, hello_world)
+        .run();
 }
 
 pub fn hello_world() {
     println!("Hello World");
 }
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Fred".to_string())));
+    commands.spawn((Person, Name("Manon".to_string())));
+    commands.spawn((Person, Name("Mclilzee".to_string())));
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
