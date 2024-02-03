dc8a79f4e7ee110217d460612efcbc350aeb3cbd
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-03 09:42:17 +0100

    Fix timer error

--- src/plugins/spammer_plugin.rs
+++ src/plugins/spammer_plugin.rs
@@ -21,23 +21,23 @@ impl Plugin for SpammerPlugins {
 
 fn spawn_spammer(
     mut commands: Commands,
+    mut spawn_timer: ResMut<SpammerSpawnTimer>,
     mut query: Query<&Transform, With<Player>>,
-    spawn_timer: Res<SpammerSpawnTimer>,
+    time: Res<Time>,
 ) {
-    if !spawn_timer.timer.just_finished() {
-        return;
+    spawn_timer.timer.tick(time.delta());
+    if spawn_timer.timer.just_finished() {
+        let mut random = rand::thread_rng();
+        let player_transform = query.single_mut();
+        let x = player_transform.translation.x + 20. + random.gen_range(0.0..20.0);
+        let y = player_transform.translation.y;
+
+        let sprite = SpriteBundle {
+            transform: Transform::from_xyz(x, y, 0.0),
+            visibility: Visibility::Visible,
+            ..default()
+        };
+
+        commands.spawn((sprite, Velocity(Vec2::new(50.0, 0.0)), Spammer, Hp(5)));
     }
-
-    let mut random = rand::thread_rng();
-    let player_transform = query.single_mut();
-    let x = player_transform.translation.x + 20. + random.gen_range(0.0..20.0);
-    let y = player_transform.translation.y;
-
-    let sprite = SpriteBundle {
-        transform: Transform::from_xyz(x, y, 0.0),
-        visibility: Visibility::Visible,
-        ..default()
-    };
-
-    commands.spawn((sprite, Velocity(Vec2::new(50.0, 0.0)), Spammer, Hp(5)));
 }
