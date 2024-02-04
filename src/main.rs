be3eb6cb87242dfaf19ed7205e800437f4ef948c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-04 11:04:19 +0100

    Add camera following player

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::{
     input::common_conditions::input_toggle_active, prelude::*, render::camera::ScalingMode,
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use components::Player;
 use plugins::default_plugins::CustomDefaultPlugin;
 use plugins::player_plugin::PlayerPlugin;
 use plugins::spammer_plugin::SpammerPlugins;
@@ -18,6 +19,7 @@ fn main() {
             WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
         )
         .add_systems(Startup, setup)
+        .add_systems(Update, follow_player)
         .run();
 }
 
@@ -29,4 +31,14 @@ fn setup(mut commands: Commands) {
         min_height: 600.0,
     };
     commands.spawn(camera);
+}
+
+fn follow_player(
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+    player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
+) {
+    let mut camera_transform = camera_query.single_mut();
+    let player_transform = player_query.single();
+
+    camera_transform.translation = player_transform.translation;
 }
