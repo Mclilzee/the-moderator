4a75c83712d8fc54cd3760d1ce4b55dc6822b1ce
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-18 23:49:44 +0100

    Use state to spawn and dispawn boxes

--- src/debugging/player_box.rs
+++ src/debugging/player_box.rs
@@ -17,8 +17,24 @@ pub struct PlayerBoxPlugin;
 impl Plugin for PlayerBoxPlugin {
     fn build(&self, app: &mut App) {
         app.add_state::<BoxDebugState>()
-            .add_systems(Update, spawn_debug_boxes)
-            .add_systems(Update, dispawn_debug_boxes);
+            .add_systems(
+                Update,
+                toggle_state.run_if(input_just_pressed(KeyCode::ControlLeft)),
+            )
+            .add_systems(OnEnter(BoxDebugState::On), spawn_debug_boxes)
+            .add_systems(OnExit(BoxDebugState::Off), dispawn_debug_boxes);
+    }
+}
+
+fn toggle_state(mut state: ResMut<NextState<BoxDebugState>>) {
+    if let Some(current) = state.0 {
+        let next = if current == BoxDebugState::On {
+            BoxDebugState::Off
+        } else {
+            BoxDebugState::On
+        };
+
+        state.set(next);
     }
 }
 
