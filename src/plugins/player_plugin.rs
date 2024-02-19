b3a686d049051f8fc7207f98141309e01835343b
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-19 11:52:31 +0100

    Fix migration issues

--- src/plugins/player_plugin.rs
+++ src/plugins/player_plugin.rs
@@ -32,29 +32,31 @@ fn spawn_player(mut commands: Commands, asset_loader: Res<AssetsLoader>) {
 
     let animation_atlas = asset_loader.player_textures.get(&AnimationType::Idle);
     if let Some(sheet) = animation_atlas {
-        char.0.movable_object.sprite_sheet.texture_atlas = sheet.texture_atlas.to_owned();
+        char.0.movable_object.sprite_sheet.atlas.layout = sheet.texture_atlas.to_owned();
     }
 
     commands.spawn((char, Name::new("Player")));
 }
 
 fn movement(
-    keys: Res<Input<KeyCode>>,
+    keys: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Velocity, &mut Jumps, &mut Transform), With<Player>>,
     time: Res<Time>,
 ) {
     let (mut player_velocity, mut available_jumps, mut transform) = query.single_mut();
     let mut velocity = Vec3::new(0.0, player_velocity.translation.y, 0.0);
 
-    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+    if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
         velocity.x = PLAYER_SPEED;
     }
 
-    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+    if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
         velocity.x = -PLAYER_SPEED;
     }
 
-    if keys.any_just_pressed([KeyCode::Up, KeyCode::W, KeyCode::Space]) && available_jumps.0 >= 1 {
+    if keys.any_just_pressed([KeyCode::ArrowUp, KeyCode::KeyW, KeyCode::Space])
+        && available_jumps.0 >= 1
+    {
         velocity.y = PLAYER_JUMP_HEIGHT + GRAVITY_SPEED;
         available_jumps.0 -= 1;
     }
@@ -72,10 +74,10 @@ fn movement(
 }
 
 fn animate_sprite(
-    mut player_query: Query<&mut TextureAtlasSprite, With<Player>>,
-    input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut TextureAtlas, With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
     asset_loader: Res<AssetsLoader>,
 ) {
     let player_texture = player_query.single_mut();
-    if input.any_pressed([KeyCode::Left, KeyCode::D]) {}
+    if input.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyD]) {}
 }
