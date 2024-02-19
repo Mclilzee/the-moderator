10dcb218fc2208aa49581cdb6ca32fa9bc4ce4e4
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-19 11:57:40 +0100

    Rename add state to init state

--- src/debugging/player_box.rs
+++ src/debugging/player_box.rs
@@ -16,7 +16,7 @@ pub struct PlayerBoxPlugin;
 
 impl Plugin for PlayerBoxPlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<BoxDebugState>()
+        app.init_state::<BoxDebugState>()
             .add_systems(
                 Update,
                 toggle_state.run_if(input_just_pressed(KeyCode::ControlLeft)),
