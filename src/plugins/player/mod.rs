8de95c4dddc318adf10bc1ec7d927223767ca0ae
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-28 23:54:25 +0100

    Add loading player texture

--- src/plugins/player/mod.rs
+++ src/plugins/player/mod.rs
@@ -20,14 +20,28 @@ impl Plugin for PlayerPlugin {
     }
 }
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let char = (
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut atlas_server: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let mut char = (
         Character::new(PLAYER_STARING_HP, Vec2::splat(30.0)),
         Player,
         Jumps(ALLOWED_JUMPS),
     );
 
-    let model: Handle<Image> = asset_server.load("knight/all.png");
+    let texture: Handle<Image> = asset_server.load("knight/all.png");
+    let layout = atlas_server.add(TextureAtlasLayout::from_grid(
+        Vec2::new(32.0, 32.0),
+        7,
+        3,
+        None,
+        None,
+    ));
+
+    char.0.movable_object.sprite_sheet.texture = texture;
+    char.0.movable_object.sprite_sheet.atlas = TextureAtlas { layout, index: 1 };
 
     commands.spawn((char, Name::new("Player")));
 }
