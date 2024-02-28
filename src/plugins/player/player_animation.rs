6b2f52e24902807fe9a474928aabeb766f21a43e
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-28 23:14:43 +0100

    Add animations to player

--- src/plugins/player/player_animation.rs
+++ src/plugins/player/player_animation.rs
@@ -3,15 +3,17 @@ use bevy::prelude::*;
 #[derive(Resource)]
 pub struct PlayerAnimation {
     atlas: TextureAtlas,
+    idle: Animation,
+    moving: Animation,
 }
 
 pub struct Animation {
-    first_frame: u32,
-    last_frame: u32,
+    pub first_frame: u32,
+    pub last_frame: u32,
 }
 
 impl Animation {
-    fn new(first_frame: u32, last_frame: u32) -> Self {
+    pub fn new(first_frame: u32, last_frame: u32) -> Self {
         Animation {
             first_frame,
             last_frame,
