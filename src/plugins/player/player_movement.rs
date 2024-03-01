6a512889db9027e0ce9242da93a8ec4a2b2b123a
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-01 08:29:34 +0100

    Add animation timer

--- src/plugins/player/player_movement.rs
+++ src/plugins/player/player_movement.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use crate::{
     components::Velocity,
     consts::{GRAVITY_ACCELERATION, GRAVITY_MAX_SPEED},
+    AnimationTimer,
 };
 
 use super::*;
@@ -45,10 +46,15 @@ pub fn movement(
     }
 }
 
-fn animation(
+pub fn animate(
     mut atlas_query: Query<&mut TextureAtlas, With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     mut timer: ResMut<AnimationTimer>,
 ) {
+    let mut atlas = atlas_query.single_mut();
+    timer.0.tick(time.delta());
+    if timer.0.finished() {
+        atlas.index = ((atlas.index + 1) % 7) + 1;
+    }
 }
