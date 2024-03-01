bf63da93a3de15d3eda5790f3a38875fbd5333b2
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-01 07:41:36 +0100

    Add asset loader

--- src/plugins/asset_loader.rs
+++ src/plugins/asset_loader.rs
@@ -1,17 +1,39 @@
-use bevy::prelude::*;
+use std::ops::Range;
 
-#[derive(Resource)]
-pub struct AssetMap {
+use bevy::{prelude::*, utils::HashMap};
+
+#[derive(Resource, Default)]
+pub struct AnimationMap(HashMap<ComponentType, Animation>);
+
+pub enum ComponentType {
+    Player,
+    Spammer,
 }
 
-
 pub enum AnimationType {
-    Player,
-    Spammer
+    Idle,
+    Running,
+}
+
+pub struct Animation {
+    atlas: TextureAtlasLayout,
+    range: HashMap<AnimationType, Range<u32>>,
 }
 
 pub struct AssetLoaderPlugin;
 impl Plugin for AssetLoaderPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
+        app.insert_resource(AnimationMap::default());
     }
+}
+
+fn setup(asset_server: Res<AssetServer>, mut atlas_server: ResMut<Assets<TextureAtlasLayout>>) {
+    let texture: Handle<Image> = asset_server.load("knight/Knight-Sheet.png");
+    let layout = atlas_server.add(TextureAtlasLayout::from_grid(
+        Vec2::new(31.0, 38.0),
+        18,
+        1,
+        None,
+        None,
+    ));
 }
