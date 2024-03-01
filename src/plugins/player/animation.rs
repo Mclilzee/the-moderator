fb2d24a8de5c3dfa0bc1acceeb57423630580df6
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-01 09:56:31 +0100

    Add animation

--- src/plugins/player/animation.rs
+++ src/plugins/player/animation.rs
@@ -3,14 +3,39 @@ use bevy::prelude::*;
 use crate::{components::Player, AnimationTimer};
 
 pub fn animate(
-    mut atlas_query: Query<&mut TextureAtlas, With<Player>>,
+    mut atlas_query: Query<(&mut TextureAtlas, &mut Sprite), With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     mut timer: ResMut<AnimationTimer>,
 ) {
-    let mut atlas = atlas_query.single_mut();
+    let (mut atlas, mut sprite) = atlas_query.single_mut();
+    let first_frame;
+    let last_frame;
+    if keys.any_pressed([
+        KeyCode::ArrowLeft,
+        KeyCode::KeyA,
+        KeyCode::ArrowRight,
+        KeyCode::KeyD,
+    ]) {
+        first_frame = 8;
+        last_frame = 17;
+        sprite.flip_x = keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]);
+    } else {
+        first_frame = 1;
+        last_frame = 7;
+    }
+
     timer.0.tick(time.delta());
     if timer.0.finished() {
-        atlas.index = ((atlas.index + 1) % 7) + 1;
+        let mut index = (atlas.index + 1) % last_frame;
+
+        if index > last_frame || index < first_frame {
+            index = first_frame;
+        }
+        atlas.index = index;
+        info!(
+            "First Frame {}, Last Frame {}, Index {}",
+            first_frame, last_frame, atlas.index
+        );
     }
 }
