c4f02ed014b3d8f91eca7cf96edb15a060a42d24
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-01 07:51:28 +0100

    Finalize asset loader

--- src/plugins/asset_loader.rs
+++ src/plugins/asset_loader.rs
@@ -3,32 +3,39 @@ use std::ops::Range;
 use bevy::{prelude::*, utils::HashMap};
 
 #[derive(Resource, Default)]
-pub struct AnimationMap(HashMap<ComponentType, Animation>);
+pub struct AnimationMap(HashMap<AnimationKey, Animation>);
 
-pub enum ComponentType {
+#[derive(Eq, Hash, PartialEq)]
+pub enum AnimationKey {
     Player,
     Spammer,
 }
 
+#[derive(Eq, Hash, PartialEq)]
 pub enum AnimationType {
     Idle,
     Running,
 }
 
 pub struct Animation {
-    atlas: TextureAtlasLayout,
-    range: HashMap<AnimationType, Range<u32>>,
+    pub atlas: Handle<TextureAtlasLayout>,
+    pub range: HashMap<AnimationType, Range<u32>>,
 }
 
 pub struct AssetLoaderPlugin;
 impl Plugin for AssetLoaderPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.insert_resource(AnimationMap::default());
+        app.insert_resource(AnimationMap::default())
+            .add_systems(PreStartup, load_assets);
     }
 }
 
-fn setup(asset_server: Res<AssetServer>, mut atlas_server: ResMut<Assets<TextureAtlasLayout>>) {
-    let texture: Handle<Image> = asset_server.load("knight/Knight-Sheet.png");
+fn load_assets(
+    asset_server: Res<AssetServer>,
+    mut atlas_server: ResMut<Assets<TextureAtlasLayout>>,
+    mut animations: ResMut<AnimationMap>,
+) {
+    let texture: Handle<Image> = asset_server.load("knight/Knight.png");
     let layout = atlas_server.add(TextureAtlasLayout::from_grid(
         Vec2::new(31.0, 38.0),
         18,
@@ -36,4 +43,13 @@ fn setup(asset_server: Res<AssetServer>, mut atlas_server: ResMut<Assets<Texture
         None,
         None,
     ));
+    let mut range: HashMap<AnimationType, Range<u32>> = HashMap::new();
+    range.insert(AnimationType::Idle, 1..7);
+
+    let range = Animation {
+        atlas: layout,
+        range,
+    };
+
+    animations.0.insert(AnimationKey::Player, range);
 }
