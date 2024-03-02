cc4ab150401571945d0d6bc7baf4006d11cf93ec
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-02 12:23:38 +0100

    Adjust platform size

--- src/plugins/platform/mod.rs
+++ src/plugins/platform/mod.rs
@@ -12,7 +12,7 @@ fn spawn_ground(mut commands: Commands) {
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::PURPLE,
-            custom_size: Some(Vec2::new(200.0, 50.0)),
+            custom_size: Some(Vec2::new(400.0, 200.0)),
             ..default()
         },
         ..default()
