262af8f4bd2e7eaced76a5da2c40b799d332adae
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-03 02:29:20 +0100

    Refactor collider

--- src/plugins/physics/collider.rs
+++ src/plugins/physics/collider.rs
@@ -1,90 +1,68 @@
 use bevy::math::Vec2;
 
-pub struct PlatformCollider {
-    entity: Option<Bounderies>,
-    platform: Bounderies,
+pub enum CollidePosition {
+    Top,
+    Bottom,
+    Left,
+    Right,
 }
 
-struct Bounderies {
+pub struct PlatformCollider {
     left: f32,
     right: f32,
     top: f32,
     bottom: f32,
-    height_offset: f32,
-    width_offset: f32,
 }
 
 impl PlatformCollider {
     pub fn new(transform: &Vec2, size: &Vec2) -> Self {
-        PlatformCollider {
-            entity: None,
-            platform: Bounderies::new(transform, size),
-        }
-    }
-
-    pub fn entity(&mut self, transform: &Vec2, size: &Vec2) {
-        self.entity = Some(Bounderies::new(transform, size));
-    }
-
-    pub fn entity_position(&self) -> Vec2 {}
-
-    fn between_left_and_right(&self) -> bool {
-        match &self.entity {
-            Some(entity) => entity.right > self.platform.left && entity.left < self.platform.right,
-            None => false,
-        }
-    }
-
-    fn between_top_and_bottom(&self) -> bool {
-        match &self.entity {
-            Some(entity) => entity.top > self.platform.bottom && entity.bottom < self.platform.top,
-            None => false,
-        }
-    }
-
-    fn colliding_top(&self) -> bool {
-        match &self.entity {
-            Some(entity) => entity.bottom < self.platform.top && entity.top > self.platform.top,
-            None => false,
-        }
-    }
-
-    fn colliding_bottom(&self) -> bool {
-        match &self.entity {
-            Some(entity) => {
-                entity.top > self.platform.bottom && entity.bottom < self.platform.bottom
-            }
-            None => false,
-        }
-    }
-
-    fn colliding_left(&self) -> bool {
-        match &self.entity {
-            Some(entity) => entity.right > self.platform.left && entity.left < self.platform.left,
-            None => false,
-        }
-    }
-
-    fn colliding_right(&self) -> bool {
-        match &self.entity {
-            Some(entity) => entity.left < self.platform.right && entity.right > self.platform.right,
-            None => false,
-        }
-    }
-}
-
-impl Bounderies {
-    fn new(transform: &Vec2, size: &Vec2) -> Self {
         let height_offset = size.x / 2.0;
         let width_offset = size.y / 2.0;
 
-        Bounderies {
+        PlatformCollider {
             left: transform.x - width_offset,
             right: transform.x + width_offset,
             top: transform.y + height_offset,
             bottom: transform.y - height_offset,
-            height_offset,
-            width_offset,
         }
+    }
+
+    pub fn position(&self, transform: &Vec2, size: &Vec2) -> Option<CollidePosition> {
+        let height = size.y / 2.0;
+        let width = size.x / 2.0;
+        let left = transform.x - width;
+        let right = transform.x + width;
+        let top = transform.y + height;
+        let bottom = transform.y - height;
+
+        if self.between_left_and_right(left, right) {
+            if self.between_top_and_bottom(top, bottom) || self.colliding_top(top, bottom) {}
+        }
+
+        None
+    }
+
+    fn between_left_and_right(&self, left: f32, right: f32) -> bool {
+        right > self.left && left < self.right
+    }
+
+    fn between_top_and_bottom(&self, top: f32, bottom: f32) -> bool {
+        top > self.bottom && bottom < self.top
+    }
+
+    fn colliding_top(&self, top: f32, bottom: f32) -> bool {
+        bottom < self.top && top > self.top
+    }
+
+    fn colliding_bottom(&self, top: f32, bottom: f32) -> bool {
+        top > self.bottom && bottom < self.bottom
+    }
+
+    fn colliding_left(&self, left: f32, right: f32) -> bool {
+        right > self.left && left < self.left
+    }
+
+    fn colliding_right(&self, left: f32, right: f32) -> bool {
+        left < self.right && right > self.right
     }
 }
