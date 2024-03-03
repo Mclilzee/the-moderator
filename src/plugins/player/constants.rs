ae558b43b5e0720f568412f2389cc3b2d32c2d08
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-03 04:14:49 +0100

    Add taking height into account

--- src/plugins/player/constants.rs
+++ src/plugins/player/constants.rs
@@ -1,4 +1,4 @@
-pub const PLAYER_SPEED: f32 = 60.0;
+pub const PLAYER_SPEED: f32 = 150.0;
 pub const PLAYER_JUMP_HEIGHT: f32 = 200.0;
 pub const PLAYER_STARING_HP: i32 = 100;
 pub const ALLOWED_JUMPS: u16 = 2;
