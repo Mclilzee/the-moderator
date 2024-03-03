0568a1f24abf745fa3cb019212289c50826db281
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-03 02:08:31 +0100

    Refactor to make collider reusable

--- src/plugins/physics/collider.rs
+++ src/plugins/physics/collider.rs
@@ -1,7 +1,7 @@
 use bevy::math::Vec2;
 
 pub struct PlatformCollider {
-    entity: Bounderies,
+    entity: Option<Bounderies>,
     platform: Bounderies,
 }
 
@@ -15,42 +15,57 @@ struct Bounderies {
 }
 
 impl PlatformCollider {
-    pub fn new(
-        entity_transform: &Vec2,
-        entity_size: &Vec2,
-        platform_transform: &Vec2,
-        platform_size: &Vec2,
-    ) -> Self {
+    pub fn new(transform: &Vec2, size: &Vec2) -> Self {
         PlatformCollider {
-            entity: Bounderies::new(entity_transform, entity_size),
-            platform: Bounderies::new(platform_transform, platform_size),
+            entity: None,
+            platform: Bounderies::new(transform, size),
         }
     }
 
     pub fn entity_position(&self) -> Vec2 {}
 
     fn between_left_and_right(&self) -> bool {
-        self.entity.right > self.platform.left && self.entity.left < self.platform.right
+        match &self.entity {
+            Some(entity) => entity.right > self.platform.left && entity.left < self.platform.right,
+            None => false,
+        }
     }
 
     fn between_top_and_bottom(&self) -> bool {
-        self.entity.top > self.platform.bottom && self.entity.bottom < self.platform.top
+        match &self.entity {
+            Some(entity) => entity.top > self.platform.bottom && entity.bottom < self.platform.top,
+            None => false,
+        }
     }
 
     fn colliding_top(&self) -> bool {
-        self.entity.bottom < self.platform.top && self.entity.top > self.platform.top
+        match &self.entity {
+            Some(entity) => entity.bottom < self.platform.top && entity.top > self.platform.top,
+            None => false,
+        }
     }
 
     fn colliding_bottom(&self) -> bool {
-        self.entity.top > self.platform.bottom && self.entity.bottom < self.platform.bottom
+        match &self.entity {
+            Some(entity) => {
+                entity.top > self.platform.bottom && entity.bottom < self.platform.bottom
+            }
+            None => false,
+        }
     }
 
     fn colliding_left(&self) -> bool {
-        self.entity.right > self.platform.left && self.entity.left < self.platform.left
+        match &self.entity {
+            Some(entity) => entity.right > self.platform.left && entity.left < self.platform.left,
+            None => false,
+        }
     }
 
     fn colliding_right(&self) -> bool {
-        self.entity.left < self.platform.right && self.entity.right > self.platform.right
+        match &self.entity {
+            Some(entity) => entity.left < self.platform.right && entity.right > self.platform.right,
+            None => false,
+        }
     }
 }
 
