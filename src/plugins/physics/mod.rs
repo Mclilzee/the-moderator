a3a4ed9968dee3a93f21b80e12d48ae4d16755f2
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-06 00:35:30 +0100

    Enable debugging for player

--- src/plugins/physics/mod.rs
+++ src/plugins/physics/mod.rs
@@ -4,7 +4,7 @@ use bevy::{
 };
 
 use crate::{
-    components::{Collider, EntityState, Velocity},
+    components::{Collider, EntityState, Spammer, Velocity},
     consts::{GRAVITY_ACCELERATION, GRAVITY_MAX_SPEED},
     InGameSet,
 };
@@ -27,7 +27,7 @@ type Colliders<'a> = (
     Option<&'a mut Velocity>,
 );
 
-fn collision(mut colliders_query: Query<Colliders>) {
+fn collision(mut colliders_query: Query<Colliders, Without<Spammer>>) {
     let mut combination = colliders_query.iter_combinations_mut();
     while let Some(
         [(collider1, mut state1, mut transform1, mut velocity1), (collider2, mut state2, mut transform2, mut velocity2)],
@@ -46,27 +46,23 @@ fn collision(mut colliders_query: Query<Colliders>) {
             continue;
         }
 
-        if *state1 == EntityState::Solid {
+        if *state1 == EntityState::Solid && (*state2 != EntityState::Solid || velocity2.is_some()) {
             match find_collision_side(&first, &second) {
                 CollisionSide::Left => {
                     transform2.translation.x = transform1.translation.x - (collider2.0.x / 2.0);
-                    info!("Left");
                 }
                 CollisionSide::Right => {
                     transform2.translation.x = transform1.translation.x + (collider2.0.x / 2.0);
-                    info!("Right");
                 }
                 CollisionSide::Top => {
-                    info!("Top");
-                    transform2.translation.y = transform1.translation.y + (collider2.0.x / 2.0);
+                    transform2.translation.y = transform1.translation.y + (collider2.0.y / 2.0);
                     if let Some(mut velocity) = velocity2 {
                         velocity.0.y = 0.0;
                     }
                     *state2 = EntityState::Grounded;
                 }
                 CollisionSide::Bottom => {
-                    info!("Bottom");
-                    transform2.translation.y = transform1.translation.y - (collider2.0.x / 2.0);
+                    transform2.translation.y = transform1.translation.y - (collider2.0.y / 2.0);
                     if let Some(mut velocity) = velocity2 {
                         velocity.0.y = 0.0;
                     }
@@ -87,6 +83,7 @@ fn find_collision_side(first: &Aabb2d, second: &Aabb2d) -> CollisionSide {
     let closest = first.closest_point(second.center());
     let offset = second.center() - closest;
     let abs = offset.abs() - second.half_size();
+    println!("Offset Abs: {abs}");
 
     if first.contains(second) || abs.y > abs.x {
         if offset.y < 0.0 {
@@ -107,7 +104,7 @@ fn movement(mut actors_query: Query<MovingActors>, time: Res<Time>) {
     for (mut transform, mut velocity) in actors_query.iter_mut() {
         velocity.0.y -= GRAVITY_ACCELERATION;
         if velocity.0.y > GRAVITY_MAX_SPEED {
-            velocity.0.y = GRAVITY_MAX_SPEED;
+            velocity.0.y = -GRAVITY_MAX_SPEED;
         }
 
         transform.translation += velocity.0.extend(0.0) * time.delta_seconds();
