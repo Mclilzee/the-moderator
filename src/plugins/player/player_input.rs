73171a200dd2c37809846b3bd99d72c161ef4cb9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-09 23:38:50 +0100

    Animation now depends on state

--- src/plugins/player/player_input.rs
+++ src/plugins/player/player_input.rs
@@ -1,20 +1,31 @@
-use crate::components::{AvailableJumps, Grounded, Player, Velocity};
+use crate::components::{AvailableJumps, EntityState, Grounded, Player, Velocity};
 use bevy::prelude::*;
 
 use super::constants::{PLAYER_JUMP_HEIGHT, PLAYER_MAX_JUMPS, PLAYER_SPEED};
 
 pub fn input(
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&mut Grounded, &mut Velocity, &mut AvailableJumps), With<Player>>,
+    mut query: Query<
+        (
+            &mut Grounded,
+            &mut Velocity,
+            &mut AvailableJumps,
+            &mut EntityState,
+        ),
+        With<Player>,
+    >,
 ) {
-    let (mut grounded, mut velocity, mut jumps) =
+    let (mut grounded, mut velocity, mut jumps, mut state) =
         query.get_single_mut().expect("Player should exist");
     velocity.0.x = 0.0;
+    *state = EntityState::Idle;
     if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+        *state = EntityState::Running;
         velocity.0.x = PLAYER_SPEED;
     }
 
     if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+        *state = EntityState::Running;
         velocity.0.x = -PLAYER_SPEED;
     }
 
