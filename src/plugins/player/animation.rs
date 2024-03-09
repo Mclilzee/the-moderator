fa43af248f9d0b9c0fa602a9d55a5d63437ff28a
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-09 23:36:52 +0100

    Make animation adaptable

--- src/plugins/player/animation.rs
+++ src/plugins/player/animation.rs
@@ -1,25 +1,29 @@
 use bevy::prelude::*;
 
 use crate::{
-    components::{EntityState, Player, Velocity},
+    components::{EntityState, Player},
     plugins::asset_loader::{AnimationKey, AnimationMap},
     AnimationTimer,
 };
 
 pub fn animate(
-    mut sprite_query: Query<
-        (&Velocity, &mut TextureAtlas, &mut Sprite, &EntityState),
-        With<Player>,
-    >,
+    mut sprite_query: Query<(&mut TextureAtlas, &mut Sprite, &EntityState), With<Player>>,
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     mut timer: ResMut<AnimationTimer>,
     animation: Res<AnimationMap>,
 ) {
-    let (velocity, mut atlas, mut sprite, state) = sprite_query.single_mut();
+    let (mut atlas, mut sprite, state) = sprite_query.single_mut();
 
-    let mut first_frame = 0;
-    let mut last_frame = 0;
+    let player_animations = &animation
+        .0
+        .get(&AnimationKey::Player)
+        .expect("Animation to be found");
+
+    let frames = player_animations
+        .indices
+        .get(state)
+        .unwrap_or(&player_animations.default);
 
     if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
         sprite.flip_x = true;
@@ -27,23 +31,12 @@ pub fn animate(
         sprite.flip_x = false;
     }
 
-    let player_animations = &animation
-        .0
-        .get(&AnimationKey::Player)
-        .expect("Animation to be found")
-        .indices;
-
-    if let Some(indices) = player_animations.get(state) {
-        first_frame = indices.first_frame;
-        last_frame = indices.last_frame;
-    }
-
     timer.0.tick(time.delta());
     if timer.0.finished() {
         let mut index = atlas.index + 1;
 
-        if atlas.index >= last_frame || atlas.index < first_frame {
-            index = first_frame;
+        if atlas.index >= frames.last_frame || atlas.index < frames.first_frame {
+            index = frames.first_frame;
         }
 
         atlas.index = index;
