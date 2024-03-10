07734ca5109fe4c1aad49ecabecbf4439766f63e
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-03-10 13:53:21 +0100

    Update animations indices

--- src/plugins/asset_loader/spammer_assets.rs
+++ src/plugins/asset_loader/spammer_assets.rs
@@ -10,17 +10,16 @@ pub fn setup(
 ) {
     let texture: Handle<Image> = asset_server.load("monsters/Spammer.png");
     let layout = atlas_server.add(TextureAtlasLayout::from_grid(
-        Vec2::new(31.0, 38.0),
+        Vec2::new(32.0, 32.0),
         18,
         1,
         None,
         None,
     ));
 
-    let idle_animation = AnimationIndices::new(1, 7);
+    let idle_animation = AnimationIndices::new(1, 18);
     let mut range: HashMap<EntityState, AnimationIndices> = HashMap::new();
     range.insert(EntityState::Idle, idle_animation);
-    range.insert(EntityState::Running, AnimationIndices::new(8, 17));
 
     let range = Animation {
         texture,
