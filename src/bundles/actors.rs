e13f9732e338e3f476f3eb243d2707abca902fa1
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-23 09:41:01 +0200

    Attach rigit body to player

--- src/bundles/actors.rs
+++ src/bundles/actors.rs
@@ -2,21 +2,21 @@ use crate::components::Health;
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
-use super::movable_sprite::MovableSprite;
-
 #[derive(Bundle)]
 pub struct Actor {
-    pub movable_sprite: MovableSprite,
-    pub collider: Collider,
-    pub hp: Health,
+    collider: Collider,
+    hp: Health,
+    body: RigidBody,
+    pub vel: Velocity,
 }
 
 impl Actor {
     pub fn new(hp: i32, width: f32, height: f32) -> Self {
         Self {
-            movable_sprite: MovableSprite::default(),
             collider: Collider::cuboid(width, height),
             hp: Health(hp),
+            body: RigidBody::Dynamic,
+            vel: Velocity::zero(),
         }
     }
 }
