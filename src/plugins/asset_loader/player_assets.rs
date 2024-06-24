f4e2fa4ad01c33b5c862e6f19260d79cdb32aed3
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-24 13:07:21 +0200

    Fix running animation for player

--- src/plugins/asset_loader/player_assets.rs
+++ src/plugins/asset_loader/player_assets.rs
@@ -11,16 +11,16 @@ pub fn setup(
     let texture: Handle<Image> = asset_server.load("knight/Knight.png");
     let layout = atlas_server.add(TextureAtlasLayout::from_grid(
         Vec2::new(31.0, 38.0),
-        18,
+        19,
         1,
-        None,
+        Some(Vec2::new(4.0, 0.0)),
         None,
     ));
 
-    let idle_animation = AnimationIndices::new(1, 7);
+    let idle_animation = AnimationIndices::new(1, 8);
     let mut range: HashMap<EntityState, AnimationIndices> = HashMap::new();
     range.insert(EntityState::Idle, idle_animation);
-    range.insert(EntityState::Running, AnimationIndices::new(8, 17));
+    range.insert(EntityState::Running, AnimationIndices::new(9, 18));
 
     let range = Animation {
         texture,
