4a112fbf8cfdbf12f7a3cdb7b94341d9072d1734
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-25 12:07:21 +0200

    Add mouse movement tracking and drawing

--- src/plugins/mouse/mod.rs
+++ src/plugins/mouse/mod.rs
@@ -1,31 +1,81 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
-use crate::bundles::platforms::Platforms;
+use crate::components::Player;
+
+#[derive(Component)]
+struct Cursor;
 
 pub struct MousePlugin;
 
 impl Plugin for MousePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, draw);
+        app.add_systems(Update, (show_cursor, draw).chain());
+    }
+}
+
+fn show_cursor(
+    mut commands: Commands,
+    window_q: Query<&Window, With<PrimaryWindow>>,
+    camera_q: Query<(&Camera, &GlobalTransform)>,
+    cursor_entity_q: Query<Entity, With<Cursor>>,
+) {
+    if window_q.is_empty() || camera_q.is_empty() {
+        return;
+    }
+
+    for id in cursor_entity_q.iter() {
+        commands.entity(id).despawn();
+    }
+
+    let (camera, camera_transform) = camera_q.single();
+    let window = window_q.single();
+    if let Some(vec) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        commands.spawn((
+            Cursor,
+            SpriteSheetBundle {
+                transform: Transform::from_xyz(vec.x, vec.y, 0.0),
+                sprite: Sprite {
+                    color: Color::BLUE,
+                    custom_size: Some(Vec2::new(5.0, 5.0)),
+                    ..default()
+                },
+                ..default()
+            },
+        ));
     }
 }
 
 fn draw(
     mut commands: Commands,
+    window_q: Query<&Window, With<PrimaryWindow>>,
+    camera_q: Query<(&Camera, &GlobalTransform)>,
     buttons: Res<ButtonInput<MouseButton>>,
-    q_windows: Query<&Window, With<PrimaryWindow>>,
 ) {
-    let mouse_position = q_windows.single().cursor_position();
-    if mouse_position.is_none() {
+    if window_q.is_empty() || camera_q.is_empty() {
         return;
     }
-    let vec = mouse_position.unwrap();
 
-    if buttons.pressed(MouseButton::Left) {
-        commands.spawn(Platforms::cuboid(
-            Color::BLUE,
-            Vec2::ONE,
-            Transform::from_xyz(vec.x, vec.y, 0.0),
-        ));
+    let (camera, camera_transform) = camera_q.single();
+    let window = window_q.single();
+    if let Some(vec) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        if buttons.pressed(MouseButton::Left) {
+            commands.spawn(SpriteSheetBundle {
+                transform: Transform::from_xyz(vec.x, vec.y, 0.0),
+                sprite: Sprite {
+                    color: Color::BLUE,
+                    custom_size: Some(Vec2::new(5.0, 5.0)),
+                    ..default()
+                },
+                ..default()
+            });
+        }
     }
 }
