000e41177f2a70f8beb9c370e85f15acc7e9f9f5
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-27 12:38:59 +0200

    Refactor using vec z

--- src/plugins/mouse/mod.rs
+++ src/plugins/mouse/mod.rs
@@ -26,15 +26,18 @@ fn move_cursor(
     if let Some(vec) = window
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-        .map(|ray| ray.origin.truncate())
+        .map(|ray| ray.origin)
     {
         if let Ok(mut transform) = cursor_q.get_single_mut() {
-            transform.translation = vec.extend(50.0);
+            transform.translation = vec;
         } else {
             commands.spawn((
                 Cursor,
                 SpriteSheetBundle {
-                    transform: Transform::from_xyz(vec.x, vec.y, 50.0),
+                    transform: Transform {
+                        translation: vec,
+                        ..default()
+                    },
                     sprite: Sprite {
                         color: Color::BLUE,
                         custom_size: Some(Vec2::new(5.0, 5.0)),
