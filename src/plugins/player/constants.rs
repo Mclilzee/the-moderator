caee1d922d44578663aa8da0a490221bc7da5b1d
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-11-24 05:59:10 +0100

    Adjust jump height

--- src/plugins/player/constants.rs
+++ src/plugins/player/constants.rs
@@ -1,5 +1,5 @@
 pub const PLAYER_SPEED: f32 = 150.0;
-pub const PLAYER_JUMP_HEIGHT: f32 = 200.0;
+pub const PLAYER_JUMP_HEIGHT: f32 = 300.0;
 pub const PLAYER_STARING_HP: i32 = 100;
 pub const PLAYER_MAX_JUMPS: u8 = 2;
 pub const PLAYER_HEIGHT: f32 = 17.0;
