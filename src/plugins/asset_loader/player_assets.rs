6ce475b8161b963afc091e4d375e2084ea9fbf2c
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-12-03 00:02:55 +0100

    Experimental

--- src/plugins/asset_loader/player_assets.rs
+++ src/plugins/asset_loader/player_assets.rs
@@ -7,24 +7,25 @@ pub fn setup(
     asset_server: Res<AssetServer>,
     mut atlas_server: ResMut<Assets<TextureAtlasLayout>>,
     mut animations: ResMut<AnimationMap>,
+    textures: ResMut<Assets<Image>>,
 ) {
+    let mut builder = TextureAtlasBuilder::default();
     let texture: Handle<Image> = asset_server.load("fred/idle.png");
-    let layout = atlas_server.add(TextureAtlasLayout::from_grid(
-        UVec2::new(32, 32),
-        11,
-        1,
-        None,
-        None,
-    ));
+    builder.add_texture(Some(texture.id()), texture);
+    let texture: Handle<Image> = asset_server.load("fred/run.png");
+    builder.add_texture(Some(texture.id()), textures.get(texture.id()).unwrap());
+
+    let (layout, image) = builder.build().unwrap();
 
     let idle_animation = AnimationIndices::new(0, 10);
+    let run_animation = AnimationIndices::new(10, 22);
     let mut range: HashMap<EntityState, AnimationIndices> = HashMap::new();
     range.insert(EntityState::Idle, idle_animation);
-    //range.insert(EntityState::Running, AnimationIndices::new(9, 18));
+    range.insert(EntityState::Running, run_animation);
 
     let range = Animation {
-        texture,
-        atlas: layout,
+        texture: asset_server.add(image),
+        atlas: atlas_server.add(layout),
         indices: range,
         default: idle_animation,
     };
